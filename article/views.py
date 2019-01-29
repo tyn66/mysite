b4db4c65e7851d33b8b3a2be2bb37ac1858ea8b7
tyn66 <43401907+tyn66@users.noreply.github.com>
2019-01-29 16:33:26 +0800

    1

---
+++ article/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render_to_response,get_object_or_404
+from .models import Article
+# Create your views here.
+def article_detail(request,article_id):
+    article = get_object_or_404(Article, pk=article_id)  # pk为主键
+    context = {}
+    context["article_obj"] = article
+    return render_to_response("article_detail.html",context)
+
+def article_list(request):
+    articles = Article.objects.all()
+    context = {}
+    context["articles"] = articles
+    return render_to_response("article_list.html",context)
+
